b6fab3604fb18ac4d293c0196cdff96ada720486
thayron <thayronf3@gmail.com>
2020-11-07 11:54:15 -0300

    Foi feito o primerio teste usando redux mudando o valor do state min

--- src/Components/Intervalo.jsx
+++ src/Components/Intervalo.jsx
@@ -2,17 +2,20 @@ import './Intervalo.css';
 import React from 'react';
 import { connect } from 'react-redux'
 import Card from './Card';
-
+import {alterarNumeroMinimo} from '../store/numeros'
 
 function Intervalo (props) {
-    const { min, max } = props.numeros;
+    const {min, max} = props.numeros
+    
 
+    
     return (
         <Card title="Intervalo de numeros" red>
             <div className="Intervalo">
             <span>
                 <strong>Mínimo:</strong>
-                <input type="number" value={min} readOnly/>
+                <input type="number" value={min} 
+                onChange={e => props.alterarMinino(+ e.target.value)}/>
             </span>
             <span>
                 <strong>Máximo:</strong>
@@ -25,10 +28,18 @@ function Intervalo (props) {
 }
 function mapStateToProps(state) {
     return {
-
         numeros: state.numeros
     }
 
 }
-export default connect(mapStateToProps)(Intervalo);
+
+function mapActionCreatorToProp(dispatch){
+    return{
+        alterarMinino(novoNumero){
+        const action =    alterarNumeroMinimo(novoNumero)
+        dispatch(action)
+        }
+    }
+}
+export default connect(mapStateToProps,mapActionCreatorToProp)(Intervalo);
 
--- src/store/StoreConfig.js
+++ src/store/StoreConfig.js
@@ -3,10 +3,25 @@ import { createStore, combineReducers } from 'redux';
 const reducers = combineReducers({
     numeros: function(state, action){
      //   console.log(state, '', action)
-        return{
-            min: 31,
-            max: 10
-        }
+
+     switch(action.type){
+         case 'NUM_MIN_ALTERADO':
+             return{
+                 ...state,
+                 min: action.payload,
+             }
+             case 'NUM_MAX_ALTERADO':
+                 return{
+                    ...state,
+                    max: action.payload
+
+                 }
+             default:
+                 return {
+                     min: 7,
+                     max: 31
+                 }
+     }
        
     }, nomes: function(state, action){
         return[
