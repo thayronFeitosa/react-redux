b89390094441c8c24176c8160715e120d341598f
thayron <thayronf3@gmail.com>
2020-11-07 12:09:45 -0300

    Alterando o numero maximo

--- src/Components/Intervalo.jsx
+++ src/Components/Intervalo.jsx
@@ -2,25 +2,26 @@ import './Intervalo.css';
 import React from 'react';
 import { connect } from 'react-redux'
 import Card from './Card';
-import {alterarNumeroMinimo} from '../store/numeros'
+import { alterarNumeroMinimo, alterarNumeroMaximo } from '../store/numeros'
 
-function Intervalo (props) {
-    const {min, max} = props.numeros
-    
+function Intervalo(props) {
+    const { min, max } = props.numeros
 
-    
+
+
     return (
         <Card title="Intervalo de numeros" red>
             <div className="Intervalo">
-            <span>
-                <strong>Mínimo:</strong>
-                <input type="number" value={min} 
-                onChange={e => props.alterarMinino(+ e.target.value)}/>
-            </span>
-            <span>
-                <strong>Máximo:</strong>
-                <input type="number" value={max} readOnly />
-            </span>
+                <span>
+                    <strong>Mínimo:</strong>
+                    <input type="number" value={min}
+                        onChange={e => props.alterarMinino(+ e.target.value)} />
+                </span>
+                <span>
+                    <strong>Máximo:</strong>
+                    <input type="number" value={max}
+                    onChange={e => props.alterarMaximo(+e.target.value)} />
+                </span>
 
             </div>
         </Card>
@@ -33,13 +34,17 @@ function mapStateToProps(state) {
 
 }
 
-function mapActionCreatorToProp(dispatch){
-    return{
-        alterarMinino(novoNumero){
-        const action =    alterarNumeroMinimo(novoNumero)
-        dispatch(action)
+function mapDispatchToProp(dispatch) {
+    return {
+        alterarMinino(novoNumero) {
+            const action = alterarNumeroMinimo(novoNumero)
+            dispatch(action)
+        },
+        alterarMaximo(novoNumero) {
+            const action = alterarNumeroMaximo(novoNumero)
+            dispatch(action)
         }
     }
 }
-export default connect(mapStateToProps,mapActionCreatorToProp)(Intervalo);
+export default connect(mapStateToProps, mapDispatchToProp)(Intervalo);
 
